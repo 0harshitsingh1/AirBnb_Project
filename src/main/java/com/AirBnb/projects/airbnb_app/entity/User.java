c330edd7f76b76a2d079d31c5271400311f1a1bb
0harshitsingh1 <harshitsingh5225@gmail.com>
2026-02-24 20:34:13 +0530

    Week_1

---
+++ src/main/java/com/AirBnb/projects/airbnb_app/entity/User.java
@@ -0,0 +1,31 @@
+package com.AirBnb.projects.airbnb_app.entity;
+
+import com.AirBnb.projects.airbnb_app.entity.enums.Role;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.util.Set;
+
+@Entity
+@Setter
+@Getter
+@Table(name ="app_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password; //encode
+
+    private String name;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Enumerated(EnumType.STRING)
+    private Set<Role> roles;
+
+}
---
+++ src/main/java/com/AirBnb/projects/airbnb_app/entity/enums/Gender.java
@@ -0,0 +1,12 @@
+package com.AirBnb.projects.airbnb_app.entity.enums;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+public enum Gender {
+    MALE,
+    FEMALE,
+    OTHER
+
+}
